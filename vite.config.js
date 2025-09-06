75308330998fcb595d36866811f3b2dd27aea616
Outliers1211 <jeonjaehyeok1211@gmail.com>
2025-09-06 21:52:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 여기서 "username"과 "ai-debate-site"는 본인 GitHub 아이디/레포 이름으로 수정!
 export default defineConfig({
   plugins: [react()],
-  base: "/ai-debate-site/"
+  base: "/aidebate.io/"
 });
